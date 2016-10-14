8345c9ebfd5e266a39d73d914c53346a50b804e6
Jason M Harrington <jason.m.harrington@gmail.com>
2016-10-13 21:27:16 -0500

    Construct new template module based on puppet module generate

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,29 +1,48 @@
-# == Class: template
+# Class: template
+# ===========================
 #
-# Full description of class example_class here.
+# Full description of class template here.
 #
-# === Parameters
+# Parameters
+# ----------
 #
-# None
+# Document parameters here.
 #
-# === Variables
+# * `sample parameter`
+# Explanation of what this parameter affects and what it defaults to.
+# e.g. "Specify one or more upstream ntp servers as an array."
 #
-# None
+# Variables
+# ----------
 #
-# === Examples
+# Here you should define a list of variables that this module would require.
 #
-#  class { 'template': }
+# * `sample variable`
+#  Explanation of how this variable affects the function of this class and if
+#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#  External Node Classifier as a comma separated list of hostnames." (Note,
+#  global variables should be avoided in favor of class parameters as
+#  of Puppet 2.6.)
 #
-#  include template
+# Examples
+# --------
 #
-# === Authors
+# @example
+#    class { 'template':
+#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#    }
 #
-# Author Name <author@domain.tld>
+# Authors
+# -------
 #
-# === Copyright
+# Author Name <author@domain.com>
 #
-# No copyright expressed, or implied.
+# Copyright
+# ---------
+#
+# Copyright 2016 Your name here, unless otherwise noted.
 #
 class template {
+
 
 }
